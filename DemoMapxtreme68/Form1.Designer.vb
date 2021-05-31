64c201a7884fa12a192a624626ea6e064883ec3d
le quyen <lequyenk41@outlook.com>
2021-05-31 10:14:39 +0700

    Upload project Tu Anh

---
+++ DemoMapxtreme68/Form1.Designer.vb
@@ -0,0 +1,379 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.SplitContainer2 = New System.Windows.Forms.SplitContainer()
+        Me.LayerControl1 = New MapInfo.Windows.Controls.LayerControl()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.MapControl1 = New MapInfo.Windows.Controls.MapControl()
+        Me.MapToolBar1 = New MapInfo.Windows.Controls.MapToolBar()
+        Me.MapToolBarButton1 = New MapInfo.Windows.Controls.MapToolBarButton()
+        Me.MapToolBarButton2 = New MapInfo.Windows.Controls.MapToolBarButton()
+        Me.MapToolBarButton3 = New MapInfo.Windows.Controls.MapToolBarButton()
+        Me.MapToolBarButton4 = New MapInfo.Windows.Controls.MapToolBarButton()
+        Me.MapToolBarButton5 = New MapInfo.Windows.Controls.MapToolBarButton()
+        Me.MapToolBarButton6 = New MapInfo.Windows.Controls.MapToolBarButton()
+        Me.MapToolBarButton7 = New MapInfo.Windows.Controls.MapToolBarButton()
+        Me.MapToolBarButton8 = New MapInfo.Windows.Controls.MapToolBarButton()
+        Me.MapToolBarButton9 = New MapInfo.Windows.Controls.MapToolBarButton()
+        Me.MapToolBarButton10 = New MapInfo.Windows.Controls.MapToolBarButton()
+        Me.MapToolBarButton13 = New MapInfo.Windows.Controls.MapToolBarButton()
+        Me.MapToolBarButton11 = New MapInfo.Windows.Controls.MapToolBarButton()
+        Me.MapToolBarButton12 = New MapInfo.Windows.Controls.MapToolBarButton()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        CType(Me.SplitContainer2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer2.Panel1.SuspendLayout()
+        Me.SplitContainer2.Panel2.SuspendLayout()
+        Me.SplitContainer2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.SplitContainer2)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.MapControl1)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.MapToolBar1)
+        Me.SplitContainer1.Size = New System.Drawing.Size(1292, 644)
+        Me.SplitContainer1.SplitterDistance = 430
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'SplitContainer2
+        '
+        Me.SplitContainer2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer2.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer2.Name = "SplitContainer2"
+        Me.SplitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'SplitContainer2.Panel1
+        '
+        Me.SplitContainer2.Panel1.Controls.Add(Me.LayerControl1)
+        '
+        'SplitContainer2.Panel2
+        '
+        Me.SplitContainer2.Panel2.Controls.Add(Me.Button9)
+        Me.SplitContainer2.Panel2.Controls.Add(Me.Button8)
+        Me.SplitContainer2.Panel2.Controls.Add(Me.Button7)
+        Me.SplitContainer2.Panel2.Controls.Add(Me.Button6)
+        Me.SplitContainer2.Panel2.Controls.Add(Me.Button5)
+        Me.SplitContainer2.Panel2.Controls.Add(Me.CheckBox1)
+        Me.SplitContainer2.Panel2.Controls.Add(Me.Button4)
+        Me.SplitContainer2.Panel2.Controls.Add(Me.Button3)
+        Me.SplitContainer2.Panel2.Controls.Add(Me.Button2)
+        Me.SplitContainer2.Panel2.Controls.Add(Me.Button1)
+        Me.SplitContainer2.Size = New System.Drawing.Size(430, 644)
+        Me.SplitContainer2.SplitterDistance = 360
+        Me.SplitContainer2.TabIndex = 0
+        '
+        'LayerControl1
+        '
+        Me.LayerControl1.AllowDragAndDrop = True
+        Me.LayerControl1.AllowRenaming = True
+        Me.LayerControl1.AutomaticLabelRemoval = MapInfo.Windows.Controls.LayerControl.LabelRemoval.Prompt
+        Me.LayerControl1.CheckBoxes = False
+        Me.LayerControl1.ConfirmLayerRemoval = True
+        Me.LayerControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.LayerControl1.EditNameAfterInsertingLayer = True
+        Me.LayerControl1.FeatureLayerImageType = MapInfo.Windows.Controls.LayerControl.ImageType.Sample
+        Me.LayerControl1.ItemHeight = 20
+        Me.LayerControl1.Location = New System.Drawing.Point(0, 0)
+        Me.LayerControl1.Map = Nothing
+        Me.LayerControl1.Name = "LayerControl1"
+        Me.LayerControl1.OriginalMap = Nothing
+        Me.LayerControl1.PointSampleMaximumPointSize = 18.0R
+        Me.LayerControl1.SelectedObject = Nothing
+        Me.LayerControl1.SelectedTab = MapInfo.Windows.Controls.PropertiesCategory.Custom
+        Me.LayerControl1.ShowContextMenu = True
+        Me.LayerControl1.ShowMapNode = True
+        Me.LayerControl1.Size = New System.Drawing.Size(430, 360)
+        Me.LayerControl1.TabIndex = 0
+        Me.LayerControl1.Tools = Nothing
+        Me.LayerControl1.UpdateWhenCollectionChanges = True
+        Me.LayerControl1.UpdateWhenMapViewChanges = True
+        Me.LayerControl1.UpdateWhenNameChanges = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(237, 183)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(121, 30)
+        Me.Button9.TabIndex = 9
+        Me.Button9.Text = "Coordinate"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(38, 183)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(153, 30)
+        Me.Button8.TabIndex = 8
+        Me.Button8.Text = "Change Style"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(237, 143)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(121, 34)
+        Me.Button7.TabIndex = 7
+        Me.Button7.Text = "ZoomValue"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(38, 143)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(153, 34)
+        Me.Button6.TabIndex = 6
+        Me.Button6.Text = "RemoveLayer"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(237, 102)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(121, 35)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "ScaleValue"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(248, 71)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(142, 21)
+        Me.CheckBox1.TabIndex = 4
+        Me.CheckBox1.Text = "Biên giới quốc gia"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(237, 18)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(132, 32)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Show Lable"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(38, 99)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(153, 38)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Cấu hình mặc định"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(38, 56)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(153, 37)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Lưu geoset"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(38, 18)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(153, 32)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "AddLayer"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'MapControl1
+        '
+        Me.MapControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.MapControl1.IgnoreLostFocusEvent = False
+        Me.MapControl1.Location = New System.Drawing.Point(0, 28)
+        Me.MapControl1.Name = "MapControl1"
+        Me.MapControl1.Size = New System.Drawing.Size(858, 616)
+        Me.MapControl1.TabIndex = 1
+        Me.MapControl1.Text = "MapControl1"
+        Me.MapControl1.Tools.LeftButtonTool = Nothing
+        Me.MapControl1.Tools.MiddleButtonTool = Nothing
+        Me.MapControl1.Tools.RightButtonTool = Nothing
+        '
+        'MapToolBar1
+        '
+        Me.MapToolBar1.Buttons.AddRange(New System.Windows.Forms.ToolBarButton() {Me.MapToolBarButton1, Me.MapToolBarButton2, Me.MapToolBarButton3, Me.MapToolBarButton4, Me.MapToolBarButton5, Me.MapToolBarButton6, Me.MapToolBarButton7, Me.MapToolBarButton8, Me.MapToolBarButton9, Me.MapToolBarButton10, Me.MapToolBarButton13, Me.MapToolBarButton11, Me.MapToolBarButton12})
+        Me.MapToolBar1.DropDownArrows = True
+        Me.MapToolBar1.Location = New System.Drawing.Point(0, 0)
+        Me.MapToolBar1.MapControl = Me.MapControl1
+        Me.MapToolBar1.Name = "MapToolBar1"
+        Me.MapToolBar1.ShowToolTips = True
+        Me.MapToolBar1.Size = New System.Drawing.Size(858, 28)
+        Me.MapToolBar1.TabIndex = 0
+        '
+        'MapToolBarButton1
+        '
+        Me.MapToolBarButton1.ButtonType = MapInfo.Windows.Controls.MapToolButtonType.[Select]
+        Me.MapToolBarButton1.Name = "MapToolBarButton1"
+        Me.MapToolBarButton1.ToolTipText = "Select"
+        '
+        'MapToolBarButton2
+        '
+        Me.MapToolBarButton2.ButtonType = MapInfo.Windows.Controls.MapToolButtonType.Pan
+        Me.MapToolBarButton2.Name = "MapToolBarButton2"
+        Me.MapToolBarButton2.ToolTipText = "Pan"
+        '
+        'MapToolBarButton3
+        '
+        Me.MapToolBarButton3.ButtonType = MapInfo.Windows.Controls.MapToolButtonType.ZoomIn
+        Me.MapToolBarButton3.Name = "MapToolBarButton3"
+        Me.MapToolBarButton3.ToolTipText = "Zoom-in"
+        '
+        'MapToolBarButton4
+        '
+        Me.MapToolBarButton4.ButtonType = MapInfo.Windows.Controls.MapToolButtonType.ZoomOut
+        Me.MapToolBarButton4.Name = "MapToolBarButton4"
+        Me.MapToolBarButton4.ToolTipText = "Zoom-out"
+        '
+        'MapToolBarButton5
+        '
+        Me.MapToolBarButton5.ButtonType = MapInfo.Windows.Controls.MapToolButtonType.SelectRectangle
+        Me.MapToolBarButton5.Name = "MapToolBarButton5"
+        Me.MapToolBarButton5.ToolTipText = "Marquee Select"
+        '
+        'MapToolBarButton6
+        '
+        Me.MapToolBarButton6.ButtonType = MapInfo.Windows.Controls.MapToolButtonType.SelectRadius
+        Me.MapToolBarButton6.Name = "MapToolBarButton6"
+        Me.MapToolBarButton6.ToolTipText = "Radius Select"
+        '
+        'MapToolBarButton7
+        '
+        Me.MapToolBarButton7.ButtonType = MapInfo.Windows.Controls.MapToolButtonType.SelectPolygon
+        Me.MapToolBarButton7.Name = "MapToolBarButton7"
+        Me.MapToolBarButton7.ToolTipText = "Polygon Select"
+        '
+        'MapToolBarButton8
+        '
+        Me.MapToolBarButton8.ButtonType = MapInfo.Windows.Controls.MapToolButtonType.AddPoint
+        Me.MapToolBarButton8.Name = "MapToolBarButton8"
+        Me.MapToolBarButton8.ToolTipText = "Add Point"
+        '
+        'MapToolBarButton9
+        '
+        Me.MapToolBarButton9.ButtonType = MapInfo.Windows.Controls.MapToolButtonType.AddLine
+        Me.MapToolBarButton9.Name = "MapToolBarButton9"
+        Me.MapToolBarButton9.ToolTipText = "Add Line"
+        '
+        'MapToolBarButton10
+        '
+        Me.MapToolBarButton10.ButtonType = MapInfo.Windows.Controls.MapToolButtonType.AddPolyline
+        Me.MapToolBarButton10.Name = "MapToolBarButton10"
+        Me.MapToolBarButton10.ToolTipText = "Add Polyline"
+        '
+        'MapToolBarButton13
+        '
+        Me.MapToolBarButton13.ButtonType = MapInfo.Windows.Controls.MapToolButtonType.AddText
+        Me.MapToolBarButton13.Name = "MapToolBarButton13"
+        Me.MapToolBarButton13.ToolTipText = "Add Text"
+        '
+        'MapToolBarButton11
+        '
+        Me.MapToolBarButton11.ButtonType = MapInfo.Windows.Controls.MapToolButtonType.AddPolygon
+        Me.MapToolBarButton11.Name = "MapToolBarButton11"
+        Me.MapToolBarButton11.ToolTipText = "Add Polygon"
+        '
+        'MapToolBarButton12
+        '
+        Me.MapToolBarButton12.ButtonType = MapInfo.Windows.Controls.MapToolButtonType.Center
+        Me.MapToolBarButton12.Name = "MapToolBarButton12"
+        Me.MapToolBarButton12.ToolTipText = "Center"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1292, 644)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.PerformLayout()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.SplitContainer2.Panel1.ResumeLayout(False)
+        Me.SplitContainer2.Panel2.ResumeLayout(False)
+        Me.SplitContainer2.Panel2.PerformLayout()
+        CType(Me.SplitContainer2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents SplitContainer2 As SplitContainer
+    Friend WithEvents MapControl1 As MapInfo.Windows.Controls.MapControl
+    Friend WithEvents MapToolBar1 As MapInfo.Windows.Controls.MapToolBar
+    Friend WithEvents MapToolBarButton1 As MapInfo.Windows.Controls.MapToolBarButton
+    Friend WithEvents MapToolBarButton2 As MapInfo.Windows.Controls.MapToolBarButton
+    Friend WithEvents MapToolBarButton3 As MapInfo.Windows.Controls.MapToolBarButton
+    Friend WithEvents MapToolBarButton4 As MapInfo.Windows.Controls.MapToolBarButton
+    Friend WithEvents MapToolBarButton5 As MapInfo.Windows.Controls.MapToolBarButton
+    Friend WithEvents MapToolBarButton6 As MapInfo.Windows.Controls.MapToolBarButton
+    Friend WithEvents MapToolBarButton7 As MapInfo.Windows.Controls.MapToolBarButton
+    Friend WithEvents MapToolBarButton8 As MapInfo.Windows.Controls.MapToolBarButton
+    Friend WithEvents MapToolBarButton9 As MapInfo.Windows.Controls.MapToolBarButton
+    Friend WithEvents MapToolBarButton10 As MapInfo.Windows.Controls.MapToolBarButton
+    Friend WithEvents MapToolBarButton13 As MapInfo.Windows.Controls.MapToolBarButton
+    Friend WithEvents MapToolBarButton11 As MapInfo.Windows.Controls.MapToolBarButton
+    Friend WithEvents MapToolBarButton12 As MapInfo.Windows.Controls.MapToolBarButton
+    Friend WithEvents LayerControl1 As MapInfo.Windows.Controls.LayerControl
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+End Class
---
+++ DemoMapxtreme68/Form1.vb
@@ -0,0 +1,210 @@
+﻿Imports System.Reflection
+Imports System.IO
+Imports MapInfo.Data
+Imports MapInfo.Mapping
+Imports MapInfo.Engine
+Imports MapInfo.Windows.Dialogs
+
+Imports System.Windows.Forms
+Imports System
+Imports System.Drawing
+Imports System.Collections
+Imports System.ComponentModel
+Imports MapInfo.Persistence
+Imports System.Data
+
+Imports MapInfo.Styles
+
+Public Class Form1
+    Inherits System.Windows.Forms.Form
+    Private _table As Table
+    Private _layer As FeatureLayer
+    Private _lineStyleDlg As LineStyleDlg = Nothing
+    Private path As String = "I:\May ao\Source Code\KCD-S-Git\KCD-S\bin\x86\Maps\Maps_55"
+    ' Private path As String = "D:\\Mayao\\Maps\\BanDoNguoc\\Maps\\"
+    ''' <summary>
+    ''' Mở Geoset
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'load geoset
+        Dim gl As New MapGeosetLoader(path + "\BanDo.gst")
+        MapControl1.Map.Load(gl)
+        LayerControl1.Map = MapControl1.Map
+        ' đặt mức zoom khi load bản đồ lên là 25000m
+
+        Me.MapControl1.Map.Zoom = New MapInfo.Geometry.Distance(25000, MapInfo.Geometry.DistanceUnit.Mile)
+
+    End Sub
+    ''' <summary>
+    ''' Mở bảng .TAB
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim path As String = Environment.CurrentDirectory + "\Map"
+        Session.Current.TableSearchPath.Path = path
+        _table = Session.Current.Catalog.OpenTable("SongSuoiA.TAB")
+        _layer = New FeatureLayer(_table)
+        MapControl1.Map.Layers.Add(_layer)
+    End Sub
+    ''' <summary>
+    ''' Lấy Tỷ lệ bản đồ
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim dblScale As Double = System.Convert.ToDouble(String.Format("{0:E2}", MapControl1.Map.Scale.ToString()))
+        MessageBox.Show("Scale=  1:" + dblScale.ToString())
+    End Sub
+    ''' <summary>
+    ''' lưu geoset. với mapxtreme 6.8 có thể sử dụng được geoset của bản mapx 5.0 nhưng không có chiều ngược lại
+    ''' Bản mapxtreme 6.8 dữ liệu worckspace lưu định dạng .wor
+    ''' In MapX, you used the Map.SaveMapAsGeoset 
+    ''' 
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim wsp As New WorkSpacePersistence()
+        wsp.Save(path + "MapSave.mws")
+        MessageBox.Show("Lưu thành công MapSave.mws")
+    End Sub
+    ''' <summary>
+    ''' Remove Layer "SongSuoiA"
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim LayerThuyHe As FeatureLayer = MapControl1.Map.Layers("SongSuoiA")
+        If Not LayerThuyHe Is Nothing Then
+            MapControl1.Map.Layers.Remove(LayerThuyHe)
+        End If
+    End Sub
+    ''' <summary>
+    ''' Giá trị zoom map
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+
+        ' Display the zoom level
+        Dim dblZoom As Double = System.Convert.ToDouble(String.Format("{0:E2}", MapControl1.Map.Zoom.Value))
+        MessageBox.Show("Zoom: " + dblZoom.ToString() + " " + MapControl1.Map.Zoom.Unit.ToString())
+    End Sub
+    ''' <summary>
+    ''' Đổi style cho lớp đối tượng
+    ''' cái này lưu ý phải add references là MapInfo.Windows.Framework (trong thư mục commonfile/mapinfo) 
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Dim _lyr As FeatureLayer = Me.MapControl1.Map.Layers("DuongBoBien")
+        'If _lineStyleDlg Is Nothing Then
+        _lineStyleDlg = New LineStyleDlg
+        'End If
+        If _lineStyleDlg.ShowDialog() = DialogResult.OK Then
+            Dim fsm As FeatureOverrideStyleModifier = New FeatureOverrideStyleModifier(Nothing, New MapInfo.Styles.CompositeStyle(_lineStyleDlg.LineStyle))
+            _lyr.Modifiers.Append(fsm)
+            Me.MapControl1.Map.Zoom = New MapInfo.Geometry.Distance(25000, MapInfo.Geometry.DistanceUnit.Mile)
+        End If
+        ' tương tự cho các đối tượng vùng, điểm, nhãn
+        'Private _areaStyleDlg As AreaStyleDlg = Nothing
+        'Private _textStyleDlg As TextStyleDlg = Nothing
+        'Private _symbolStyleDlg As SymbolStyleDlg = Nothing
+
+        'Get the layer we want
+        'Dim _lyr As FeatureLayer = Me.MapControl1.Map.Layers("world")
+        ''Create and show the style dialog
+        'If _areaStyleDlg Is Nothing Then
+        '    _areaStyleDlg = New AreaStyleDlg
+        'End If
+        '' After getting style from dialog, create and apply the featureoverridestylemodifier object to layer
+        'If _areaStyleDlg.ShowDialog() = DialogResult.OK Then
+        '    Dim fsm As FeatureOverrideStyleModifier = New FeatureOverrideStyleModifier(Nothing, New MapInfo.Styles.CompositeStyle(_areaStyleDlg.AreaStyle))
+        '    _lyr.Modifiers.Append(fsm)
+        '    Me.MapControl1.Map.Zoom = New MapInfo.Geometry.Distance(25000, MapInfo.Geometry.DistanceUnit.Mile)
+        'End If
+        '-------------
+        'Dim _lyr As MapInfo.Mapping.LabelLayer = Me.MapControl1.Map.Layers("worldlabels")
+        'If _textStyleDlg Is Nothing Then
+        '    _textStyleDlg = New TextStyleDlg
+        'End If
+        'If _textStyleDlg.ShowDialog() = DialogResult.OK Then
+        '    _lyr.Sources("world").DefaultLabelProperties.Style = New TextStyle(_textStyleDlg.FontStyle, _lyr.Sources("world").DefaultLabelProperties.Style.CalloutLine)
+        '    _lyr.Sources("worldcap").DefaultLabelProperties.Style = New TextStyle(_textStyleDlg.FontStyle, _lyr.Sources("worldcap").DefaultLabelProperties.Style.CalloutLine)
+        '    _lyr.Sources("wldcty25").DefaultLabelProperties.Style = New TextStyle(_textStyleDlg.FontStyle, _lyr.Sources("wldcty25").DefaultLabelProperties.Style.CalloutLine)
+        '    Me.MapControl1.Map.Zoom = New MapInfo.Geometry.Distance(6250, MapInfo.Geometry.DistanceUnit.Mile)
+        'End If
+        '.......................
+        'Dim _lyr As FeatureLayer = Me.MapControl1.Map.Layers("worldcap")
+        'If _symbolStyleDlg Is Nothing Then
+        '    _symbolStyleDlg = New SymbolStyleDlg
+        'End If
+        'If _symbolStyleDlg.ShowDialog() = DialogResult.OK Then
+        '    Dim fsm As FeatureOverrideStyleModifier = New FeatureOverrideStyleModifier(Nothing, New MapInfo.Styles.CompositeStyle(_symbolStyleDlg.SymbolStyle))
+        '    _lyr.Modifiers.Append(fsm)
+        '    Me.MapControl1.Map.Zoom = New MapInfo.Geometry.Distance(6250, MapInfo.Geometry.DistanceUnit.Mile)
+        'End If
+        '------------------------------
+        ' Get the layer we want
+        'Dim _lyr As FeatureLayer = Me.MapControl1.Map.Layers("worldcap")
+        ''Create a sparse point style
+        'Dim vs As MapInfo.Styles.SimpleVectorPointStyle = New SimpleVectorPointStyle
+        ''Just change the color and code and attributes flag to indicate that
+        'vs.Code = 55
+        'vs.PointSize = 25
+        'vs.Color = System.Drawing.Color.Red
+        '' vs.Attributes = StyleAttributes.PointAttributes.Color | StyleAttributes.PointAttributes.VectorCode;
+        '' And apply to the layer
+        'Dim fsm As FeatureOverrideStyleModifier = New FeatureOverrideStyleModifier(Nothing, New MapInfo.Styles.CompositeStyle(vs))
+        '_lyr.Modifiers.Append(fsm)
+        'Me.MapControl1.Map.Zoom = New MapInfo.Geometry.Distance(6250, MapInfo.Geometry.DistanceUnit.Mile)
+    End Sub
+    ''' <summary>
+    ''' Hiển thị nhãn 'name' lớp 'FIR'
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim table As Table = Session.Current.Catalog.OpenTable("D:\Mayao\Maps\BanDoNguoc\Maps\Map\FIR.tab")
+        Dim featureLayer As New FeatureLayer(table)
+        MapControl1.Map.Layers.Add(featureLayer)
+        ' label layer before the feature layer
+        Dim labelLayer As New MapInfo.Mapping.LabelLayer()
+        MapControl1.Map.Layers.Insert(0, labelLayer)
+        Dim source As New MapInfo.Mapping.LabelSource(table)
+        source.DefaultLabelProperties.Caption = "name"
+        ' Append the label source to the label layer so that it shows on the map
+        labelLayer.Sources.Append(source)
+    End Sub
+    ''' <summary>
+    ''' ẩn hiện lớp dữ liệu Biên giới quốc gia
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If (CheckBox1.Checked = True) Then
+
+            MapControl1.Map.Layers.Item("BienGioiQuocGia").Enabled = True
+        Else
+            MapControl1.Map.Layers.Item("BienGioiQuocGia").Enabled = False
+        End If
+
+    End Sub
+
+    Private Sub MapControl1_MouseDown(sender As Object, e As MouseEventArgs) Handles MapControl1.MouseDown
+
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Dim table As Table = Session.Current.Catalog.OpenTable("D:\Mayao\Maps\BanDoNguoc\Maps\Map\FIR.tab")
+
+
+        Dim featureLayer As New FeatureLayer(table)
+        Dim cor As String
+        cor = featureLayer.CoordSys.Datum.ToString()
+        MessageBox.Show(cor.ToString())
+    End Sub
+End Class
